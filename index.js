ff10ed8e906f247e647577635e377e9dfef35f02
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-05-14 20:49:56 -0400

    tweaks

--- index.js
+++ index.js
@@ -1,80 +1,107 @@
+var DISCOVER_TIMEOUT = 10 * 1000; // ms
+
 var noble = require('noble');
 var applescript = require('applescript');
 
-// Need this because calling noble.stopScanning in callback failing
-function stop() {
-    noble.stopScanning();
+var scanTimeout = null;;
+
+function startScanning() {
+    noble.startScanning([], false, function() {
+        console.log("Scan Started.");
+
+        scanTimeout = setTimeout(stopScanning, DISCOVER_TIMEOUT);
+    }); // any service UUID, no duplicates
 }
 
-noble.on('scanStart', function() {
-    console.log("Scan Started.");
-    //setTimeout(noble.stopScanning, 3000);
-    setTimeout(stop, 5000);
-});
-
-noble.on('scanStop', function() {
-    console.log("Scan Stopped.");
+function scanTimeout() {
     // TODO if we didn't find a sensor tag, log a message and exit
-});
+
+    stopScanning();
+}
+
+function stopScanning() {
+    noble.stopScanning(function() {
+        console.log("Scan Stopped.");
+
+        isScanning = false;
+    });
+}
 
 noble.on('discover', function(peripheral) {
     console.log(peripheral.toString());
 
-    // The SensorTag doesn't advertise any services, so let's filter based on name
-    if (peripheral.advertisement.localName && peripheral.advertisement.localName.match(/Sensor/)) {
+    var localName = peripheral.advertisement.localName;
+
+    // The SensorTag doesn't advertise any services, so let's filter based on local name
+    if (localName && localName.match(/Sensor/)) {
+        stopScanning(); // stop scanning, we found a Sensor Tag
+
+        clearTimeout(scanTimeout); // cancel the timeout
+
         console.log("Attempting to connect to " + peripheral.advertisement.localName);
 
-        peripheral.connect(function(error) {
-            if (error) {
-                console.log("There was an error connecting " + error);
-            }
-        });
-
-        peripheral.on('connect', function() {
-            console.log("Connected");
-
-            var serviceUUIDs = ['FFE0'];
-            var characteristicUUIDs = ['FFE1'];
-
-            peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, function(error, services, characteristics) {
-                if (error) {
-                    console.log("Error discovering services and characteristics " + error);
-                    return;
-                }
-
-                var characteristic = characteristics[0];
-                console.log("Found the characteristic " + characteristic);
-
-                characteristic.notify(true, function(error) {
-                    if (error) {
-                        console.log("Error adding notification " + error);
-                    }
-                });
-
-                // This is called when notification state changes
-                characteristic.on('notify', function(state) {
-                    console.log("Characteristic notification changed to " + state);
-                });
-
-                // This is called when the data changes
-                characteristic.on('data', function(data, isNotification) {
-                    if (isNotification) {
-                        if (data[0] === 1) {
-                            console.log("Right Arrow");
-                            rightArrow();
-                        } else if (data[0] === 2) {
-                            console.log("Left Arrow");
-                            leftArrow();
-                        }
-                    }
-                });
-
-            });
-
-        });
-
+        connectAndSetUpSensorTag(peripheral);
     }
 });
+
+function connectAndSetUpSensorTag(peripheral) {
+    peripheral.connect(function(error) {
+        if (error) {
+            console.log("There was an error connecting " + error);
+            return; // TODO: exit?
+        }
+
+        console.log("Connected");
+
+        var serviceUUIDs = ['FFE0'];
+        var characteristicUUIDs = ['FFE1'];
+
+        peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, function(error, services, characteristics) {
+            if (error) {
+                console.log("Error discovering services and characteristics " + error);
+                return; // TODO: exit?
+            }
+
+            var characteristic = characteristics[0];
+            console.log("Found the characteristic " + characteristic);
+
+            characteristic.notify(true, function(error) {
+                if (error) {
+                    console.log("Error adding notification " + error);
+                    return; // TODO: exit?
+                }
+            });
+
+            // This is called when notification state changes
+            characteristic.on('notify', function(state) {
+                console.log("Characteristic notification changed to " + state);
+            });
+
+            // This is called when the data changes
+            characteristic.on('data', onCharacteristicData);
+        });
+    });
+
+    peripheral.on('disconnect', onDisconnect); // attach disconnect event handler
+}
+
+function onDisconnect() {
+    console.log('Peripheral disconnected!')
+
+    // TODO exit?
+}
+
+function onCharacteristicData(data, isNotification) {
+    if (isNotification) {
+        if (data[0] === 1) {
+            console.log("Right Arrow");
+            rightArrow();
+        } else if (data[0] === 2) {
+            console.log("Left Arrow");
+            leftArrow();
+        }
+    }
+}
 
 function rightArrow() {
     applescript.execFile('right.applescript', function(err, rtn) {
@@ -92,4 +119,4 @@ function leftArrow() {
     });
 }
 
-noble.startScanning(); // any service UUID, no duplicates
+startScanning();
